d2f62d6a8355cab04fd01ba55b12d8356e90c34a
ducsdz <ducnahn2508@gmail.com>
2024-09-24 13:55:51 +0700

    config local applications

--- udagram-api/src/sequelize.ts
+++ udagram-api/src/sequelize.ts
@@ -1,7 +1,6 @@
 import {Sequelize} from 'sequelize-typescript';
 import {config} from './config/config';
 
-
 export const sequelize = new Sequelize({
   'username': config.username,
   'password': config.password,
@@ -9,5 +8,12 @@ export const sequelize = new Sequelize({
   'host': config.host,
 
   'dialect': config.dialect,
+  // config ssl parameter
+  "dialectOptions": {
+        "ssl": {
+            "require": true,
+            "rejectUnauthorized": false
+        }
+    },
   'storage': ':memory:',
 });
