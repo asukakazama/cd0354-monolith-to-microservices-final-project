21f62cfbfd16615bffe8646d728fa6d112b7380a
ducsdz <ducnahn2508@gmail.com>
2024-09-30 11:37:31 +0700

    update load balancer

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://acce67bb0e6f64a91acf7c80ee05b7a0-812062285.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
